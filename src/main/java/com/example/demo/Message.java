5f080f139928eea32502003d7be1038c664039ef
TL <TL@gmail.com>
2024-08-11 19:11:04 +0530

    WH-1458 this is commit 4 and added the logger code into messageRunner class

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -1,5 +1,23 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Message {
+	
+	public String messageRunner() {
+		
+		int a=10;
+		Logger logger=LoggerFactory.getLogger(Message.class);
+		
+		logger.info("This is loger inform and started");
+		System.out.println("This is the message runner class");
+		logger.info("This is the end of the program and stopped");
+		return "Message runner";
+		
+		
+	}
 
 }
