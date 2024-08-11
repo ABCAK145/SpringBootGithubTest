7da23da4d88ec9b54a39e675e1add393fcf2f4c1
TL <TL@gmail.com>
2024-08-11 17:50:54 +0530

    WH-1450-First time commit

---
+++ src/main/java/com/example/demo/SpringBootGithubTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubTestApplication.class, args);
+	}
+
+}
